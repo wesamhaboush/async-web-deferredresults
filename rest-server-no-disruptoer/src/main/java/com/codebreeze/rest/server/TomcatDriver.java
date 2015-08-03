e3e333a5f40aa599f16860724d93002575c50b01
Wesam Haboush <wesam.haboush@gmail.com>
2015-08-02 20:52:11 -0700

    upgraded tomcat and added support for nio2 to it

--- rest-server-no-disruptoer/src/main/java/com/codebreeze/rest/server/TomcatDriver.java
+++ rest-server-no-disruptoer/src/main/java/com/codebreeze/rest/server/TomcatDriver.java
@@ -5,6 +5,7 @@ import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.codebreeze.rest.server.config.AppConfig;
 import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -17,11 +18,24 @@ import java.io.File;
 public class TomcatDriver {
     public static void main(final String... args) throws Exception {
         final EchoServiceConfiguration echoServiceConfiguration = parseParamsWithJCommander(args);
+        Connector connector = new Connector();
+        connector.setProtocol("org.apache.coyote.http11.Http11Nio2Protocol");
+        connector.setAsyncTimeout(15000);
+        connector.setAttribute("acceptCount", 1000); //default is 100
+        connector.setAttribute("acceptorThreadCount", 4); //default is 1
+        connector.setAttribute("maxConnections", 10000); //default is 10000 for nio2
+        connector.setAttribute("maxThreads", 400);
+        connector.setAttribute("minSpareThreads", 100);
+        connector.setAttribute("processorCache", 400);
+        connector.setAttribute("useCaches", true); //default is false
+        connector.setPort(echoServiceConfiguration.port + 1);
+
         final Tomcat tomcat = new Tomcat();
+        tomcat.getService().addConnector(connector);
         tomcat.setPort(echoServiceConfiguration.port);
         tomcat.setSilent(false);
         final File base = new File(System.getProperty("java.io.tmpdir"));
-        final Context rootCtx = tomcat.addContext("/", base.getAbsolutePath());
+        final Context rootCtx = tomcat.addContext("", base.getAbsolutePath());
         Tomcat.addServlet(rootCtx, "springServlet", new AsyncDispatcherServlet((getContext())));
         rootCtx.addServletMapping("/*", "springServlet");
         tomcat.start();
